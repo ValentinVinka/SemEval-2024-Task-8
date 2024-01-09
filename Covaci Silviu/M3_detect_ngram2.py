63a28f5a44a7f88702081e1b9fcaf2ca0a961c15
Silviu <scovaci@istorm.ro>
2024-01-09 13:33:50 +0200

    new tests

--- Covaci Silviu/M3_detect_ngram2.py
+++ Covaci Silviu/M3_detect_ngram2.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 from sklearn.metrics import classification_report
+from sklearn.metrics import f1_score, accuracy_score
 #from sklearn.externals import joblib
 import os
 import pickle
@@ -16,24 +17,39 @@ def load_jsonl(file_path):
             data.append(obj)
     return data
 
-def train_model(train_df, test_df, model_path):
-    # Adjusting TfidfVectorizer parameters
+def get_vectorised_data(train_df, test_df):
+     # Adjusting TfidfVectorizer parameters
     tfidf_vectorizer = TfidfVectorizer(ngram_range=(1, 2))  # Using unigrams and bigrams
     # You can also explore other parameters like max_features, min_df, max_df, etc.
 
     # Concatenate training and testing data for vectorization
     all_data = pd.concat([train_df['text'], test_df['text']])
-
-    # Labeling: 0 for human, 1 for AI
-    train_labels = train_df['label']
-
     # Fit and transform using the updated vectorizer
     tfidf_vectorizer.fit(all_data)
     train_vectors = tfidf_vectorizer.transform(train_df['text'])
     test_vectors = tfidf_vectorizer.transform(test_df['text'])
-    print(test_vectors)
-    exit()
+    return train_vectors, test_vectors
+    
+def vectorise_data(df):
+     # Adjusting TfidfVectorizer parameters
+    tfidf_vectorizer = TfidfVectorizer(ngram_range=(1, 2))  # Using unigrams and bigrams
+    # You can also explore other parameters like max_features, min_df, max_df, etc.
 
+    # Concatenate training and testing data for vectorization
+    all_data = pd.concat([train_df['text'], test_df['text']])
+    # Fit and transform using the updated vectorizer
+    tfidf_vectorizer.fit(df['text'])
+    vectors = tfidf_vectorizer.transform(df['text'])
+    return vectors
+    
+def train_model(train_df, test_df, model_path):
+   
+
+    # Labeling: 0 for human, 1 for AI
+    train_labels = train_df['label']
+
+    train_vectors, test_vectors = get_vectorised_data(train_df, test_df)
+    
     # Train a simple Linear SVM classifier
     classifier = LinearSVC()
     classifier.fit(train_vectors, train_labels)
@@ -45,32 +61,31 @@ def train_model(train_df, test_df, model_path):
         pickle.dump(classifier, file)
     print("Model trained!")    
         
-def test_model(train_df, model_path, output_path):
-            
+def test_model(train_df, test_df, model_path, output_path):            
+    train_vectors, test_vectors = get_vectorised_data(train_df, test_df)
+                
     # Pentru a încărca modelul din fișier
     with open(model_path, 'rb') as file:
         classifier = pickle.load(file)
     
-    predicted = classifier.predict(test_df['text'])
-    print(predicted)
-    exit()
-    
-    predictions_df = pd.DataFrame({'id': test_df['id'], 'label': predictions})
+    predictions_df = pd.DataFrame({'id': test_df['id']})
+    predictions_df['label'] = classifier.predict(test_vectors)
     predictions_df.to_json(prediction_path, lines=True, orient='records')
+    
+    merged_df = predictions_df.merge(test_df, on='id', suffixes=('_pred', '_gold'))
+    accuracy = accuracy_score(merged_df['label_gold'], merged_df['label_pred'])
+    print("Accuracy:", accuracy)
             
     # Evaluate the classifier
-    print(classification_report(test_df['label'], predicted))
+    print(classification_report(test_df['label'], predictions_df['label']))
     
-    return predicted
+    return predictions_df
 
 absolute_path = os.path.abspath('subtaskA/data')
     
 train_path = absolute_path + '/subtaskA_train_monolingual.jsonl'
 test_path = absolute_path + '/subtaskA_dev_monolingual.jsonl'
-prediction_path = absolute_path + '/subtaskA_prediction_monolingual.jsonl'
-
-print(train_path)
-exit()
+prediction_path = absolute_path + '/subtaskA_prediction_monolingual_ngram.jsonl'
 
 # Load training and testing data
 train_data = load_jsonl(train_path)
@@ -80,11 +95,11 @@ test_data = load_jsonl(test_path)
 train_df = pd.DataFrame(train_data)
 test_df = pd.DataFrame(test_data)
 
-model_path = absolute_path +  '/m3detect_mgram.model.pkl'
+model_path = 'm3detect_mgram.model.pkl'
 prediction_path = absolute_path + '/subtaskA_prediction_monolingual3.jsonl'
 
-train_model(train_df, test_df, model_path)
-#test_model(test_df, model_path, prediction_path)
+#train_model(train_df, test_df, model_path)
+test_model(train_df, test_df, model_path, prediction_path)
 
 print("done!")
 
--- Covaci Silviu/m3_detect.py
+++ Covaci Silviu/m3_detect.py
@@ -12,14 +12,20 @@ import string
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, accuracy_score
-
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import FunctionTransformer
 from transformers import AutoTokenizer, AutoModel
 from torch.utils.data import DataLoader
 
+import pickle
 import os
 
+app_configs = {}
 
-class DetectDataset(Dataset):
+class PreprocessDataset(Dataset):
     def __init__(self, dataframe, tokenizer):
         texts = dataframe.text.values.tolist()
 
@@ -120,31 +126,17 @@ class DetectDataset(Dataset):
         return text, label
     
     
-class TextClassifier(nn.Module):
-    def __init__(self, base_model):
-        global layer1_in, layer1_out, layer2_in, layer2_out    
-        super(TextClassifier, self).__init__()
+#classifier for roberta base,bert, distilbert with 768 neurons on layer 1 and 32 on layer 2
+class CustomClassifier1(nn.Module):
+    def __init__(self, pretrained_model):
+        super(CustomClassifier1, self).__init__()
 
-        layer2_in = layer1_out
-        layer2_out = 1
-        self.bert = base_model
-        self.fc1 = nn.Linear(layer1_in, layer1_out)
-        self.fc2 = nn.Linear(layer2_in, layer2_out)
+        self.bert = pretrained_model
+        self.fc1 = nn.Linear(768, 32)
+        self.fc2 = nn.Linear(32, 1)
 
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
-        
-        #self.linear = nn.Sequential(
-        #    nn.Linear(512, 256),
-        #    nn.ReLU(inplace=True),
-        #    nn.Linear(256, 128),
-        #    nn.ReLU(inplace=True),
-        #    nn.Linear(128, 64),
-        #    nn.ReLU(inplace=True),
-        #    nn.Linear(64, layer2_out)
-        #)
-        #print('Linear: ', self.linear)
-        
         
     def forward(self, input_ids, attention_mask):
         bert_out = self.bert(input_ids=input_ids,
@@ -157,11 +149,35 @@ class TextClassifier(nn.Module):
 
         return x
 
+#classifier for roberta large with 1024 neurons on layer 1 and 8 on layer 2
+class CustomClassifier2(nn.Module):
+    def __init__(self, pretrained_model):
+        super(CustomClassifier1, self).__init__()
+
+        self.bert = pretrained_model
+        self.fc1 = nn.Linear(1024, 32)
+        self.fc2 = nn.Linear(8, 1)
+
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+        
+    def forward(self, input_ids, attention_mask):
+        bert_out = self.bert(input_ids=input_ids,
+                             attention_mask=attention_mask)[0][:, 0]
+        x = self.fc1(bert_out)
+        x = self.relu(x)
+        
+        x = self.fc2(x)
+        x = self.sigmoid(x)
+
+        return x
+    
 def target_device():
     #gpu support for Mac
     use_mps = torch.backends.mps.is_available()
     use_cuda = torch.cuda.is_available()        
-    device = torch.device("cuda" if use_cuda else "mps" if use_mps else "cpu")
+    device = torch.device("cuda" if use_cuda else "mps" if use_mps else "cpu")    
+    app_configs['device'] = device
     print("Use device:", device)
     return device
 
@@ -170,7 +186,7 @@ def train(model, train_dataloader, val_dataloader, learning_rate, epochs, model_
     early_stopping_threshold_count = 0
     
     
-    device = target_device()
+    device = app_configs['device']
     criterion = nn.BCELoss()
     optimizer = Adam(model.parameters(), lr=learning_rate)
 
@@ -242,9 +258,8 @@ def train(model, train_dataloader, val_dataloader, learning_rate, epochs, model_
                 break
 
 def get_text_predictions(model, loader):
-    device = target_device()
+    device = app_configs['device']
     model = model.to(device)
-    
     
     results_predictions = []
     with torch.no_grad():
@@ -261,36 +276,110 @@ def get_text_predictions(model, loader):
     
     return torch.cat(results_predictions).cpu().detach().numpy()
     
-def create_and_train(train_path, tokenizer, base_model, model_name):
-    # Load JSON file with dataset. Perform basic transformations.
+def get_pretrained_model():
+    global app_configs
+    
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+    tokenizer = AutoTokenizer.from_pretrained(app_configs['base_model'])
+    pretrained_model = AutoModel.from_pretrained(app_configs['base_model'])
+    
+    app_configs['tokenizer'] = tokenizer
+    app_configs['pretrained_model'] = pretrained_model
+    return tokenizer, pretrained_model
+    
+#Funcție pentru a obține reprezentări DistilBERT pentru un text
+def get_distilbert_embedding(text):
+    tokens = app_configs['tokenizer'](text, return_tensors='pt')
+    # Mutarea datelor pe GPU
+    tokens = {key: value.to(app_configs['device']) for key, value in tokens.items()}
+    with torch.no_grad():
+        outputs = app_configs['pretrained_model'](**tokens)
+    return outputs.last_hidden_state.mean(dim=1).squeeze()
+   
+def get_data(train_path, test_path, random_seed):
+    """
+    function to read dataframe with columns
+    """
+
     train_df = pd.read_json(train_path, lines=True)
+    test_df = pd.read_json(test_path, lines=True)
+    
+    train_df, val_df = train_test_split(train_df, test_size=0.2, stratify=train_df['label'], random_state=random_seed)
+
+    percentOfData = 10
+    train_df = train_df.sample(int(len(train_df)*percentOfData/100))
+    val_df = val_df.sample(int(len(val_df)*percentOfData/100))
+    test_df = test_df.sample(int(len(test_df)*percentOfData/100))
+    print(len(train_df))
+    print(len(val_df))
+    print(len(test_df))
+    return train_df, val_df, test_df    
+
+def pipeline_train(traid_df):
+    # Exemplu de date
+    texts = traid_df['text']
+    labels = traid_df['label']
+
+    # Divizați setul de date în antrenare și testare
+    X_train, X_test, y_train, y_test = train_test_split(texts, labels, test_size=0.2, random_state=42)
+
+    # Construiți un pipeline cu TfidfVectorizer și un clasificator
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+            ('tfidf', TfidfVectorizer()),
+            ('distilbert', FunctionTransformer(get_distilbert_embedding, validate=False))
+        ])),
+        ('classifier', LogisticRegression())
+    ])
+    # Mutarea întregului pipeline pe GPU
+    #pipeline.named_steps['classifier'].to(app_configs['device'])
+
+    # Antrenați pipeline-ul pe datele de antrenare
+    pipeline.fit(X_train, y_train)   
+    # Salvați modelul într-un fișier
+    with open(app_configs['model_name'] + ".pipeline", 'wb') as file:
+        pickle.dump(pipeline, file)
+        
+    #joblib.dump(pipeline, app_configs['model_name'] + ".pipeline")
+    
+def create_and_train():
+    #global app_configs
+       
+    # Load JSON file with dataset. Perform basic transformations.
+    train_df = pd.read_json(app_configs['train_path'], lines=True)
     train_df, val_df = train_test_split(train_df, test_size=0.2, random_state=42)
 
     train_df = train_df.drop(["model", "source"], axis=1)
     val_df = val_df.drop(["model", "source"], axis=1)
     
+    target_device()    
+    tokenizer, pretrained_model = get_pretrained_model()
         
-    train_dataloader = DataLoader(DetectDataset(train_df, tokenizer), batch_size=8, shuffle=True, num_workers=0)
-    val_dataloader = DataLoader(DetectDataset(val_df, tokenizer), batch_size=8, num_workers=0)
+    train_dataloader = DataLoader(PreprocessDataset(train_df, tokenizer), batch_size=8, shuffle=True, num_workers=0)
+    val_dataloader = DataLoader(PreprocessDataset(val_df, tokenizer), batch_size=8, num_workers=0)
 
-    model = TextClassifier(base_model)
-
-
-    learning_rate = 1e-5
-    epochs = 5
-    train(model, train_dataloader, val_dataloader, learning_rate, epochs, model_name)
+    if (app_configs['pipeline']):
+        pipeline_train(train_df)
+    else:
+        myModel = app_configs['classifier'](pretrained_model)
+        train(myModel, train_dataloader, val_dataloader, app_configs['learning_rate'], app_configs['epochs'], app_configs['model_name'])
     
-def load_and_evaluate(test_path, prediction_path, tokenizer, model_name):
-    test_df = pd.read_json(test_path, lines=True)
+def load_and_evaluate():
+    #global app_configs
+    
+    test_df = pd.read_json(app_configs['test_path'], lines=True)
     test_df = test_df.drop(["model", "source"], axis=1)
     
-    model = torch.load(model_name + ".pt")
+    target_device()
+    tokenizer, pretrained_model = get_pretrained_model()
+    
+    model = torch.load(app_configs['model_name'] + ".pt")
     
     predictions_df = pd.DataFrame({'id': test_df['id']})
-    test_dataloader = DataLoader(DetectDataset(test_df, tokenizer), batch_size=8, shuffle=False, num_workers=0)
+    test_dataloader = DataLoader(PreprocessDataset(test_df, tokenizer), batch_size=8, shuffle=False, num_workers=0)
     predictions_df['label'] = get_text_predictions(model, test_dataloader)
     #
-    predictions_df.to_json(prediction_path, lines=True, orient='records')
+    predictions_df.to_json(app_configs['prediction_path'], lines=True, orient='records')
     merged_df = predictions_df.merge(test_df, on='id', suffixes=('_pred', '_gold'))
     accuracy = accuracy_score(merged_df['label_gold'], merged_df['label_pred'])
     print("Accuracy:", accuracy)
@@ -301,23 +390,38 @@ np.random.seed(0)
 absolute_path = os.path.abspath('subtaskA/data')
 
 
-#layer1_in = 768 #for distilbert, roberta-base
-#layer1_out = 32 
+distilbert_model_configs1 = {
+    'base_model': 'distilbert-base-uncased',
+    'classifier': CustomClassifier1,
+    'pipeline': 1,
+    'model_prefix': '3mdetect_pipeline'
+}
 
-layer1_in = 1024 #for roberta large
-layer1_out = 8
+robertabase_model_configs1 = {
+    'base_model': 'roberta-large',
+    'classifier': CustomClassifier1, 
+}
 
-BERT_MODEL = "roberta-large"
-model_name="best_roberta-large"
-#model_name="best_distilbert"
-#BERT_MODEL = "distilbert-base-uncased"
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
-tokenizer = AutoTokenizer.from_pretrained(BERT_MODEL)
-base_model = AutoModel.from_pretrained(BERT_MODEL)
+robertalarge_model_configs1 = {
+    'base_model': 'roberta-large',
+    
+    'classifier': CustomClassifier2, 
+}
 
-test_path = absolute_path + '/subtaskA_dev_monolingual.jsonl'
-prediction_path = absolute_path + '/subtaskA_prediction_monolingual_pytorch_' + BERT_MODEL + '.jsonl'
-load_and_evaluate(test_path, prediction_path, tokenizer, model_name)
+default_configs = {
+    'model_prefix': '3mdetect',
+    'learning_rate': 1e-5,
+    'epochs': 5
+}
 
-#train_path = absolute_path + '/subtaskA_train_monolingual.jsonl'
-#create_and_train(train_path, tokenizer, base_model, model_name)
+app_configs = default_configs.copy()
+app_configs.update(distilbert_model_configs1)
+
+app_configs['model_name'] = app_configs['model_prefix'] + "_" + app_configs['base_model']
+app_configs['train_path'] = absolute_path + '/subtaskA_train_monolingual.jsonl'
+app_configs['test_path'] = absolute_path + '/subtaskA_dev_monolingual.jsonl'
+app_configs['prediction_path'] = absolute_path + '/predictions/subtaskA_prediction_monolingual_' + app_configs['model_name'] + '.jsonl'
+
+print("Working on pretrained-model:", app_configs['base_model'])
+create_and_train()
+#load_and_evaluate()
